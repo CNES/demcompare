a2bf445048d566f16168f21908c835d9b758f8b7
Emmanuel Dubois <emmanuel.dubois@cnes.fr>
2021-01-25 23:28:05 +0100

    fix: change version system management to setuptools_scm + clean

--- setup.py
+++ setup.py
@@ -1,33 +1,52 @@
+# Install dem_compare, whether via
+#      ``python setup.py install``
+#    or
+#      ``pip install dem_compare``
+"""
+This file is the dem_compare package main program.
+"""
+
 from setuptools import setup
 from subprocess import check_output
-from codecs import open
+from codecs import open as copen
 
-
-requirements = ['numpy',
+# Meta-data.
+NAME = 'dem_compare'
+DESCRIPTION = 'A tool to compare Digital Elevation Models'
+URL = 'https://github.com/CNES/dem_compare'
+AUTHOR = 'CNES'
+REQUIRES_PYTHON = '>=3.6.0'
+EMAIL = 'julien.michel@cnes.fr'
+LICENSE = 'Apache License 2.0'
+REQUIREMENTS = ['numpy',
                 'scipy',
                 'pygdal=={}.*'.format(check_output(['gdal-config', '--version']).rstrip().decode("utf-8")),
                 'pyproj',
                 'matplotlib',
                 'astropy',
-                'sphinx', 
-                'lib_programname']
+                'sphinx',
+                'lib_programname',
+                'setuptools-scm']
 
+SETUP_REQUIREMENTS = ['setuptools-scm']
 
 def readme():
-    with open('readme.md', "r", "utf-8") as f:
-        return f.read()
+    with copen('readme.md', "r", "utf-8") as fstream:
+        return fstream.read()
 
-
-setup(name='dem_compare',
-      version_format='{sha}',
-      include_package_data = True,
-      setup_requires=['very-good-setuptools-git-version'], 
-      description='A tool to compare Digital Elevation Models',
-      long_description=readme(),
-      url='https://framagit.org/jmichel-otb/dem_compare',
-      author='CNES',
-      author_email='julien.michel@cnes.fr',
-      license='GNU LGPLv3',
-      packages=['dem_compare'],
-      scripts=['cli/cli-dem_compare.py', 'cli/compare_with_baseline.py'],
-      install_requires=requirements)
+setup(
+    name=NAME,
+    use_scm_version=True,
+    description=DESCRIPTION,
+    url=URL,
+    author=AUTHOR,
+    author_email=EMAIL,
+    license=LICENSE,
+    packages=['dem_compare'],
+    include_package_data = True,
+    long_description=readme(),
+    install_requires=REQUIREMENTS,
+    python_requires=REQUIRES_PYTHON,
+    setup_requires=SETUP_REQUIREMENTS,
+    scripts=['cli/cli-dem_compare.py', 'cli/compare_with_baseline.py']
+)
