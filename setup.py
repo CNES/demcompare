b07b71afdf075b5c9e09a0b02964c187de6e1966
Emmanuel Dubois <emmanuel.dubois@cnes.fr>
2021-02-24 15:09:14 +0100

    fix: clean setup_requires dependencies not needed

--- setup.py
+++ setup.py
@@ -9,14 +9,11 @@ Packaging setup.py for compatibility
 All packaging in setup.cfg, except setuptools_scm version
 """
 
-import pkg_resources
 import setuptools
-
-pkg_resources.require('setuptools>=42', "wheel", "setuptools_scm[toml]>=3.4")
 
 try:
     setuptools.setup(use_scm_version=True)
-except:  
+except:
     print(
         "\n\nAn error occurred while building the project, "
         "please ensure you have the most updated version of setuptools, "
