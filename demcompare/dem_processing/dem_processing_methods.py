087eefa139e0b69dd0fd33b3decce33ba084b9ce
Nathan Lalloué <Nathan.lalloue@noveltis.fr>
2023-07-25 11:57:08 +0200

    199-noveltis-curvature-implementation: resolve threads

--- demcompare/dem_processing/dem_processing_methods.py
+++ demcompare/dem_processing/dem_processing_methods.py
@@ -212,10 +212,17 @@ class RefCurvature(DemProcessingTemplate):
         self.colorbar_title = "Curvature"
 
     def compute_curvature_filtering(
-        self, dem: xr.Dataset, k: float, rep: bool
+        self,
+        dem: xr.Dataset,
+        filter_intensity: float = 0.9,
+        replication: bool = True,
     ) -> xr.Dataset:
         """
-        Return the curvature of the input dem
+        Return the curvature of the input dem.
+        First, compute the FFT of the input dem: F(y) = FFT(DEM).
+        Then, apply a filter y^filter_intensity with s=0.9: F(y) = F(y)* y^filter_intensity. # noqa: E501, B950 # pylint: disable=line-too-long
+        Finally, apply the inverse FFT: IFFT(F(y)).
+        We keep the real part (imaginary part = digital noise).
 
         :param dem: dem xr.DataSet containing :
 
@@ -224,10 +231,13 @@ class RefCurvature(DemProcessingTemplate):
                 - classification_layer_masks : 3D (row, col, indicator)
                   xr.DataArray
         :type dem: xr.Dataset
-        :param k: DEM filter intensity. Should be close to 1.
-        :type k: float
-        :param rep: if true the image is replicated by x4
-        :type rep: bool
+        :param filter_intensity: parameter of the DEM's FFT filter intensity.
+                                 Should be close to 1.
+                                 Default = 0.9.
+        :type filter_intensity: float
+        :param replication: if true, the image is replicated by x4 in order to improve resolution. # noqa: E501, B950 # pylint: disable=line-too-long
+                            Default = True.
+        :type replication: bool
         :return: curvature xr.DataSet containing :
 
                 - image : 2D (row, col) xr.DataArray float32
@@ -247,7 +257,7 @@ class RefCurvature(DemProcessingTemplate):
 
         high, wide = dem["image"].data.shape
 
-        if rep:
+        if replication:
             data_all = np.hstack([data_all, np.flip(data_all, axis=1)])
             data_all = np.vstack([data_all, np.flip(data_all, axis=0)])
             f_y, f_x = self.calc_spatial_freq_2d(2 * high, 2 * wide, edge=np.pi)
@@ -256,7 +266,7 @@ class RefCurvature(DemProcessingTemplate):
             f_y, f_x = self.calc_spatial_freq_2d(high, wide, edge=np.pi)
 
         # spatial frequency (module)
-        spatial_freq = (f_x**2 + f_y**2) ** (k / 2)
+        spatial_freq = (f_x**2 + f_y**2) ** (filter_intensity / 2)
         spatial_freq = ifftshift(spatial_freq)
 
         image = fft2(data_all)
@@ -264,7 +274,7 @@ class RefCurvature(DemProcessingTemplate):
 
         image_filtered = ifft2(image_filtered)
 
-        if rep:
+        if replication:
             image_filtered = image_filtered[:high, :wide]
 
         image_filtered[no_data_location] = dem.attrs["nodata"]
@@ -275,8 +285,10 @@ class RefCurvature(DemProcessingTemplate):
             nodata=dem.attrs["nodata"],
             img_crs=dem.crs,
             bounds=dem.bounds,
-            classification_layer_masks=dem["classification_layer_masks"],
-            ref_slope=dem["ref_slope"],
+            classification_layer_masks=dem["classification_layer_masks"]
+            if hasattr(dem, "classification_layer_masks")
+            else None,
+            ref_slope=dem["ref_slope"] if hasattr(dem, "ref_slope") else None,
         )
 
     def neighbour_interpol(
@@ -402,4 +414,4 @@ class RefCurvature(DemProcessingTemplate):
             )
             raise ValueError
 
-        return self.compute_curvature_filtering(dem_1, 0.9, True)
+        return self.compute_curvature_filtering(dem_1)
